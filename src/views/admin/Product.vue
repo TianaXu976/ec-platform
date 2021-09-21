a17b28baa94371ca94660e5546eb7401a1618b26
Tiana Xu <xuyedda706@gmail.com>
2021-09-21 16:13:58 +0800

    [feat] pagination

--- src/views/admin/Product.vue
+++ src/views/admin/Product.vue
@@ -4,7 +4,7 @@
     <div class="addProduct">
       <button @click="handleDialog({ type: 'CREATE' })">新增產品</button>
     </div>
-    <el-table :data="productList" stripe style="width: 100%">
+    <el-table :data="paginationData" stripe style="width: 100%">
       <template slot="empty"> 尚無資料 </template>
       <el-table-column prop="category" label="類別" width="180">
       </el-table-column>
@@ -34,22 +34,37 @@
         </template>
       </el-table-column>
     </el-table>
+    <div class="block">
+      <el-pagination
+        layout="prev, pager, next"
+        :total="productList.length"
+        :page-size="pageSize"
+        @current-change="handleCurrentChange"
+      >
+      </el-pagination>
+    </div>
   </div>
 </template>
 
 <script>
-// import FormDialog from "../../components/FormDialog";
-// import { uuid } from "vue-uuid";
 import { mapMutations, mapState } from "vuex";
-// import { DIALOG_TYPE } from "../../store/productDialog"
 
 export default {
   name: "Product",
   data() {
-    return {};
+    return {
+      page: 1,
+      pageSize: 5,
+    };
   },
   computed: {
     ...mapState("productInfo", ["productList"]),
+    paginationData() {
+      return this.productList.slice(
+        (this.page - 1) * this.pageSize,
+        this.page * this.pageSize
+      );
+    },
   },
   methods: {
     ...mapMutations("productDialog", ["handleDialog"]),
@@ -62,14 +77,10 @@ export default {
       });
     },
     handleDelete(index, row) {
-      this.$confirm(
-        "刪除後就沒了喔",
-        "確定？",
-        {
-          confirmButtonText: "刪除",
-          cancelButtonText: "取消",
-        }
-      )
+      this.$confirm("刪除後就沒了喔", "確定？", {
+        confirmButtonText: "刪除",
+        cancelButtonText: "取消",
+      })
         .then(() => {
           this.handleProduct({
             type: "DELETE",
@@ -82,12 +93,15 @@ export default {
         })
         .catch(() => {});
     },
+    handleCurrentChange(val) {
+      this.page = val;
+    },
   },
 };
 </script>
 
 
-<style lang="scss" >
+<style lang="scss" scope >
 h2 {
   text-align: left;
   margin-bottom: 40px;
@@ -112,17 +126,13 @@ h4 {
   text-align: left;
 }
 
-.addForm {
-  width: 70%;
-  margin: auto;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  label {
-    padding: 20px;
+.el-table {
+  min-height: 400px;
+  .el-table__cell {
+    text-align: center;
   }
-}
-.el-table .el-table__cell {
-  text-align: center;
+  &::before{
+    display: none;
+  }
 }
 </style>
